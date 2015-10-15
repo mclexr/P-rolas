f730276204effea4a39d9a369eac71f85613d280
Michael Fidelis <mclexr@gmail.com>
2015-10-15 20:08:40 -0300

    CORS

--- app/middlewares/cors.middleware.js
+++ app/middlewares/cors.middleware.js
@@ -2,8 +2,8 @@ module.exports = function (app, router) {
 
     router.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, X-authentication, X-client");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-authentication, X-client");
         next();
     });
 
