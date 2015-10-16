daefab29d2afa0f20bc4f5cc9d2ce0268feab4d7
Michael Fidelis <mclexr@gmail.com>
2015-10-16 00:50:57 -0300

    Update headers.middleware.js

--- app/middlewares/headers.middleware.js
+++ app/middlewares/headers.middleware.js
@@ -3,6 +3,7 @@ module.exports = function (app, router) {
     router.use(function (req, res, next) {
         res.header("Content-Type", "application/json; charset=utf-8");
         res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
